c1c474ef2759204d80733b5b6fce79385ae85ae0
ppate343 <71097041+ppate343@users.noreply.github.com>
2023-06-01 13:55:36 -0400

    section css + event listeners

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector('header');
+    var sticky = navbar.offsetTop;
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
+
+
+
+
+
 // Add the "show" class to the home section when it comes into view
 window.addEventListener('DOMContentLoaded', function () {
     var homeSection = document.getElementById('home');
     var sectionPosition = homeSection.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight / 1.3; // Adjust the divisor as needed
+    var screenPosition = window.innerHeight; // Adjust the divisor as needed
 
     if (sectionPosition < screenPosition) {
         homeSection.classList.add('show');
@@ -25,7 +42,7 @@ window.addEventListener('scroll', function () {
 window.addEventListener('scroll', function () {
     var aboutSection = document.getElementById('about');
     var sectionPosition = aboutSection.getBoundingClientRect().top;
-    var screenPosition = window.innerHeight / 1.3; // Adjust the divisor as needed
+    var screenPosition = window.innerHeight; // Adjust the divisor as needed
 
     if (sectionPosition < screenPosition) {
         aboutSection.classList.add('show');
